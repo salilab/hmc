7b89a59d21ea738036def5c436da93aa3f6d958e
Seth Axen <seth.axen@gmail.com>
2019-08-30 14:34:18 -0700

    Add helper functionto warm-up and run HMC

--- pyext/src/defaults.py
+++ pyext/src/defaults.py
@@ -9,11 +9,12 @@ def setup_warmup_hmc(
     hmc_type="dynamic",
     max_depth=10,
     metric="diag",
-    save_samples=False,
-    nadapt=2000,
+    save_warmup=False,
+    warmup_optimizer_states=[],
+    nadapt=1000,
     adapt_delta=0.8,
     log_freq=0.1,
-    verbose=True,
+    verbose=False,
     shuffle=False,
     shuffle_sigma=1,
 ):
@@ -31,8 +32,9 @@ def setup_warmup_hmc(
         hmc_type=hmc_type,
         max_depth=max_depth,
         metric=metric,
-        save_samples=save_samples,
+        save_samples=save_warmup,
     )
+    hmc.add_optimizer_states(warmup_optimizer_states)
 
     if nadapt > 0:
         adaptor = Adaptor(hmc, nadapt=nadapt, adapt_delta=adapt_delta)
@@ -42,5 +44,26 @@ def setup_warmup_hmc(
         hmc.stats = None
         hmc.samples = None
         return hmc, adapt_stats, adapt_samples
+
+    return hmc
+
+
+def setup_warmup_run_hmc(
+    sf,
+    nsample=2000,
+    save_samples=True,
+    sample_optimizer_states=[],
+    **warmup_kwargs
+):
+    hmc = setup_warmup_hmc(sf, **warmup_kwargs)
+    if "nadapt" not in warmup_kwargs or warmup_kwargs["nadapt"] > 0:
+        hmc, _, _ = hmc
+
+    print("Sampling from HMC for {0} steps.".format(nsample))
+    hmc.set_save_samples(save_samples)
+    hmc.add_optimizer_states(sample_optimizer_states)
+    hmc.optimize(nsample)
+
+    hmc.stats.log_mean()
 
     return hmc
