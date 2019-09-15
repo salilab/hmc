0ba39a53439f129043673a8dddcafd8c9f4c59fb
Seth Axen <seth.axen@gmail.com>
2019-09-14 23:04:51 -0700

    Add placeholder test for Travis

---
+++ test/test_placeholder.py
@@ -0,0 +1,13 @@
+import IMP
+import IMP.test
+import IMP.hmc
+
+
+class Tests(IMP.test.TestCase):
+
+    def test_true(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    IMP.test.main()
